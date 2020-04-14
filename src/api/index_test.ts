c21e1eb4c13aafcf1dde234a51446e0a02a48209
Matthew Mueller <mattmuelle@gmail.com>
2020-04-14 18:20:15 +0200

    wrap prisma 1 AST so it's easier to create translation rules

--- src/api/index_test.ts
+++ src/api/index_test.ts
@@ -2,9 +2,19 @@ import P1 from '../p1'
 
 describe('@default value is missing', () => {
   it('should translate', () => {
-    P1.parse(`
-
+    const p1 = P1.parse(`
+      type User {
+        isActive: Boolean! @default(value: false)
+      }
     `)
-    console.log('translating!')
+    console.log(p1.objectTypeDefinitions[0].fields[0].name)
+    console.log(p1.objectTypeDefinitions[0].fields[0].directives[0].name)
+    console.log(
+      p1.objectTypeDefinitions[0].fields[0].directives[0].arguments[0].name
+    )
+    console.log(
+      p1.objectTypeDefinitions[0].fields[0].directives[0].arguments[0].value
+    )
+    // console.log('translating')
   })
 })
--- src/p1/index.ts
+++ src/p1/index.ts
@@ -1,4 +1,42 @@
-import { DocumentNode, DefinitionNode } from 'graphql/language/ast'
+import {
+  DocumentNode,
+  OperationDefinitionNode,
+  FragmentDefinitionNode,
+  SchemaDefinitionNode,
+  DirectiveDefinitionNode,
+  SchemaExtensionNode,
+  ScalarTypeDefinitionNode,
+  ObjectTypeDefinitionNode,
+  InterfaceTypeDefinitionNode,
+  UnionTypeDefinitionNode,
+  EnumTypeDefinitionNode,
+  InputObjectTypeDefinitionNode,
+  ScalarTypeExtensionNode,
+  ObjectTypeExtensionNode,
+  InterfaceTypeExtensionNode,
+  UnionTypeExtensionNode,
+  EnumTypeExtensionNode,
+  InputObjectTypeExtensionNode,
+  DefinitionNode,
+  FieldDefinitionNode,
+  NamedTypeNode,
+  ListTypeNode,
+  NonNullTypeNode,
+  TypeNode,
+  DirectiveNode,
+  ArgumentNode,
+  VariableNode,
+  IntValueNode,
+  FloatValueNode,
+  StringValueNode,
+  BooleanValueNode,
+  NullValueNode,
+  EnumValueNode,
+  ListValueNode,
+  ObjectValueNode,
+  ValueNode,
+} from 'graphql/language/ast'
+
 import { parse } from 'graphql/language/parser'
 
 export default class P1 {
@@ -10,10 +48,402 @@ export default class P1 {
   private constructor(private readonly doc: DocumentNode) {}
 
   get definitions(): Definition[] {
-    return this.doc.definitions.map((def) => new Definition(this.doc, def))
+    return this.doc.definitions.map((def) => {
+      switch (def.kind) {
+        case 'SchemaDefinition':
+          return new SchemaDefinition(def)
+        case 'ScalarTypeDefinition':
+          return new ScalarTypeDefinition(def)
+        case 'ObjectTypeDefinition':
+          return new ObjectTypeDefinition(def)
+        case 'InterfaceTypeDefinition':
+          return new InterfaceTypeDefinition(def)
+        case 'UnionTypeDefinition':
+          return new UnionTypeDefinition(def)
+        case 'EnumTypeDefinition':
+          return new EnumTypeDefinition(def)
+        case 'InputObjectTypeDefinition':
+          return new InputObjectTypeDefinition(def)
+        case 'DirectiveDefinition':
+          return new DirectiveDefinition(def)
+        case 'SchemaExtension':
+          return new SchemaExtension(def)
+        case 'ScalarTypeExtension':
+          return new ScalarTypeExtension(def)
+        case 'ObjectTypeExtension':
+          return new ObjectTypeExtension(def)
+        case 'InterfaceTypeExtension':
+          return new InterfaceTypeExtension(def)
+        case 'UnionTypeExtension':
+          return new UnionTypeExtension(def)
+        case 'EnumTypeExtension':
+          return new EnumTypeExtension(def)
+        case 'InputObjectTypeExtension':
+          return new InputObjectTypeExtension(def)
+        case 'OperationDefinition':
+          return new OperationDefinition(def)
+        case 'FragmentDefinition':
+          return new FragmentDefinition(def)
+      }
+    })
+  }
+
+  get objectTypeDefinitions(): ObjectTypeDefinition[] {
+    let arr: ObjectTypeDefinition[] = []
+    for (let def of this.definitions) {
+      if (def instanceof ObjectTypeDefinition) {
+        arr.push(def)
+      }
+    }
+    return arr
   }
 }
 
-class Definition {
-  constructor(readonly _doc: DocumentNode, readonly _def: DefinitionNode) {}
+interface Definition {
+  kind: DefinitionNode['kind']
+}
+
+class SchemaDefinition {
+  constructor(private readonly def: SchemaDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class ScalarTypeDefinition {
+  constructor(private readonly def: ScalarTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class ObjectTypeDefinition {
+  constructor(private readonly def: ObjectTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+  get name() {
+    return this.def.name.value
+  }
+  get fields(): FieldDefinition[] {
+    if (!this.def.fields) return []
+    return this.def.fields.map((field) => new FieldDefinition(field))
+  }
+}
+class InterfaceTypeDefinition {
+  constructor(private readonly def: InterfaceTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class UnionTypeDefinition {
+  constructor(private readonly def: UnionTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class EnumTypeDefinition {
+  constructor(private readonly def: EnumTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class InputObjectTypeDefinition {
+  constructor(private readonly def: InputObjectTypeDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class DirectiveDefinition {
+  constructor(private readonly def: DirectiveDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class SchemaExtension {
+  constructor(private readonly def: SchemaExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class ScalarTypeExtension {
+  constructor(private readonly def: ScalarTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class ObjectTypeExtension {
+  constructor(private readonly def: ObjectTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class InterfaceTypeExtension {
+  constructor(private readonly def: InterfaceTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class UnionTypeExtension {
+  constructor(private readonly def: UnionTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class EnumTypeExtension {
+  constructor(private readonly def: EnumTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+class InputObjectTypeExtension {
+  constructor(private readonly def: InputObjectTypeExtensionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class OperationDefinition {
+  constructor(private readonly def: OperationDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class FragmentDefinition {
+  constructor(private readonly def: FragmentDefinitionNode) {}
+  get kind() {
+    return this.def.kind
+  }
+}
+
+class FieldDefinition {
+  constructor(private readonly def: FieldDefinitionNode) {}
+
+  get name() {
+    return this.def.name.value
+  }
+
+  get type(): Type {
+    switch (this.def.type.kind) {
+      case 'ListType':
+        return new ListType(this.def.type)
+      case 'NamedType':
+        return new NamedType(this.def.type)
+      case 'NonNullType':
+        return new NonNullType(this.def.type)
+    }
+  }
+
+  get directives(): Directive[] {
+    if (!this.def.directives) return []
+    return this.def.directives.map((dir) => new Directive(dir))
+  }
+}
+
+interface Type {
+  kind: TypeNode['kind']
+}
+
+class NamedType {
+  constructor(private readonly def: NamedTypeNode) {}
+  get name() {
+    return this.def.name
+  }
+  get kind(): 'NamedType' {
+    return this.def.kind
+  }
+}
+
+class ListType {
+  constructor(private readonly def: ListTypeNode) {}
+  get kind(): 'ListType' {
+    return this.def.kind
+  }
+  inner() {
+    switch (this.def.type.kind) {
+      case 'ListType':
+        return new ListType(this.def.type)
+      case 'NamedType':
+        return new NamedType(this.def.type)
+      case 'NonNullType':
+        return new NonNullType(this.def.type)
+    }
+  }
+}
+
+class NonNullType {
+  constructor(private readonly def: NonNullTypeNode) {}
+  get kind(): 'NonNullType' {
+    return this.def.kind
+  }
+  inner() {
+    switch (this.def.type.kind) {
+      case 'ListType':
+        return new ListType(this.def.type)
+      case 'NamedType':
+        return new NamedType(this.def.type)
+    }
+  }
+}
+
+class Directive {
+  constructor(private readonly def: DirectiveNode) {}
+  get name() {
+    return this.def.name.value
+  }
+  get arguments() {
+    if (!this.def.arguments) return []
+    return this.def.arguments.map((arg) => new Argument(arg))
+  }
+}
+
+class Argument {
+  constructor(private readonly def: ArgumentNode) {}
+  get name() {
+    return this.def.name.value
+  }
+  get value(): Value {
+    switch (this.def.value.kind) {
+      case 'Variable':
+        return new Variable(this.def.value)
+      case 'IntValue':
+        return new IntValue(this.def.value)
+      case 'FloatValue':
+        return new FloatValue(this.def.value)
+      case 'StringValue':
+        return new StringValue(this.def.value)
+      case 'BooleanValue':
+        return new BooleanValue(this.def.value)
+      case 'NullValue':
+        return new NullValue(this.def.value)
+      case 'EnumValue':
+        return new EnumValue(this.def.value)
+      case 'ListValue':
+        return new ListValue(this.def.value)
+      case 'ObjectValue':
+        return new ObjectValue(this.def.value)
+    }
+  }
+}
+
+interface Value {
+  kind: ValueNode['kind']
+}
+
+class Variable {
+  constructor(private readonly def: VariableNode) {}
+
+  get kind(): 'Variable' {
+    return this.def.kind
+  }
+
+  get name(): string {
+    return this.def.name.value
+  }
+}
+
+class IntValue {
+  constructor(private readonly def: IntValueNode) {}
+
+  get kind(): 'IntValue' {
+    return this.def.kind
+  }
+
+  get value(): number {
+    return parseInt(this.def.value, 10)
+  }
+}
+
+class FloatValue {
+  constructor(private readonly def: FloatValueNode) {}
+
+  get kind(): 'FloatValue' {
+    return this.def.kind
+  }
+
+  get value(): number {
+    return parseFloat(this.def.value)
+  }
+}
+
+class StringValue {
+  constructor(private readonly def: StringValueNode) {}
+
+  get kind(): 'StringValue' {
+    return this.def.kind
+  }
+
+  get value(): string {
+    return this.def.value
+  }
+}
+
+class BooleanValue {
+  constructor(private readonly def: BooleanValueNode) {}
+
+  get kind(): 'BooleanValue' {
+    return this.def.kind
+  }
+
+  get value(): boolean {
+    return this.def.value
+  }
+}
+
+class NullValue {
+  constructor(private readonly def: NullValueNode) {}
+
+  get kind(): 'NullValue' {
+    return this.def.kind
+  }
+
+  get value(): null {
+    return null
+  }
+}
+
+class EnumValue {
+  constructor(private readonly def: EnumValueNode) {}
+
+  get kind(): 'EnumValue' {
+    return this.def.kind
+  }
+
+  get value(): string {
+    return this.def.value
+  }
+}
+
+class ListValue {
+  constructor(private readonly def: ListValueNode) {}
+  get kind(): 'ListValue' {
+    return this.def.kind
+  }
+
+  get value(): Readonly<Value[]> {
+    return this.def.values
+  }
+}
+
+type ObjectMap = { [name: string]: Value }
+
+class ObjectValue {
+  constructor(private readonly def: ObjectValueNode) {}
+  get kind(): 'ObjectValue' {
+    return this.def.kind
+  }
+
+  get value(): ObjectMap {
+    const map: ObjectMap = {}
+    for (let field of this.def.fields) {
+      map[field.name.value] = field.value
+    }
+    return map
+  }
 }
