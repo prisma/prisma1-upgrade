8f5f0c74cc5aafb5d2148133fab349c391c26168
Divyendu Singh <singh@prisma.io>
2020-05-19 20:59:51 +0530

    fix: build config
    1. Remove per-example docker-compose in favor of per database
    docker-compose
    1. Convention that Prisma service name is derived from the example
    folder name (with some slugification because some folders had a . in them)
    1. Wrap `docker-compose down` in try-catch-finally

--- scripts/sqldump.js
+++ scripts/sqldump.js
@@ -2,10 +2,17 @@ const fs = require('fs').promises
 const exec = require('execa')
 const path = require('path')
 
+const slugifyBase = require('slugify')
+
+const slugify = (str) => {
+  return slugifyBase(str).replace(/\./g, '-')
+}
+
 const root = path.join(__dirname, '..')
 const prisma1 = path.join(root, 'node_modules/.bin/prisma1')
 const prisma2 = path.join(root, 'node_modules/.bin/prisma')
 const examples = path.join(root, 'examples')
+const dcConfigPath = path.join(root, 'scripts/docker-compose-configs')
 const cwd = __dirname
 
 function sleep(ms) {
@@ -13,73 +20,87 @@ function sleep(ms) {
 }
 
 async function main() {
-  const exampleDirs = await fs.readdir(examples)
-  for (let example of exampleDirs) {
-    const fullpath = path.join(examples, example)
-    const dcfile = path.join(fullpath, 'docker-compose.yml')
-    console.log(`${example}: starting docker compose`)
-    await exec('docker-compose', ['-f', dcfile, 'up', '-d'], { cwd })
-    await sleep(20000)
-    console.log(`${example}: deploying Prisma 1 schema`)
-    const prismayml = path.join(fullpath, 'prisma.yml')
-    let stdio
-    try {
-      stdio = await exec(prisma1, ['deploy', '-p', prismayml], { cwd })
-    } catch (err) {
-      if (!~err.stdout.indexOf('Your Prisma endpoint is live')) {
-        throw err
-      }
+  const dcConfigDirs = await fs.readdir(dcConfigPath)
+  try {
+    for (dcConfigDir of dcConfigDirs) {
+      const fullpath = path.join(dcConfigPath, dcConfigDir)
+      const dcfile = path.join(fullpath, 'docker-compose.yml')
+      console.log(`starting docker compose ${dcConfigDir}`)
+      await exec('docker-compose', ['-f', dcfile, 'up', '-d'], { cwd })
+      await sleep(20000)
     }
-    await sleep(20000)
-    console.log(`${example}: dumping MySQL schema`)
-    let dump = ''
-    if (~example.indexOf(`mysql-`)) {
-      const { stdout } = await exec(`mysqldump`, [
-        `-u`,
-        `root`,
-        `-pprisma`,
-        `--port`,
-        `3307`,
-        `--host`,
-        `0.0.0.0`,
-        `prisma@dev`,
-        `--no-data`,
-      ])
-      dump = stdout
-    } else if (~example.indexOf(`postgres-`)) {
-      const { stdout } = await exec(`mysqldump`, [
-        `-u`,
-        `root`,
-        `-pprisma`,
-        `--port`,
-        `3307`,
-        `--host`,
-        `0.0.0.0`,
-        `prisma@dev`,
-        `--no-data`,
-      ])
-      dump = stdout
-    } else {
-      throw new Error('unknown database type')
-    }
-    await fs.writeFile(path.join(fullpath, 'dump.sql'), dump)
-    const schemaPrisma = path.join(fullpath, 'schema.prisma')
-    console.log(`${example}: introspecting MySQL database`)
-    await exec(
-      prisma2,
-      [
-        'introspect',
-        '--url',
-        `mysql://root:prisma@0.0.0.0:3307/prisma@dev`,
-        `--schema`,
-        schemaPrisma,
-      ],
-      {
-        cwd,
+
+    const exampleDirs = await fs.readdir(examples)
+    for (let example of exampleDirs) {
+      const fullpath = path.join(examples, example)
+      console.log(`${example}: deploying Prisma 1 schema`)
+      const prismayml = path.join(fullpath, 'prisma.yml')
+      let stdio
+      try {
+        stdio = await exec(prisma1, ['deploy', '-p', prismayml], { cwd })
+      } catch (err) {
+        if (!~err.stdout.indexOf('Your Prisma endpoint is live')) {
+          throw err
+        }
       }
-    )
-    console.log(`${example}: shutting down docker compose`)
-    await exec('docker-compose', ['-f', dcfile, 'down'], { cwd })
+      await sleep(20000)
+      console.log(`${example}: dumping MySQL schema`)
+      let dump = ''
+      if (~example.indexOf(`mysql-`)) {
+        const { stdout } = await exec(`mysqldump`, [
+          `-u`,
+          `root`,
+          `-pprisma`,
+          `--port`,
+          `3307`,
+          `--host`,
+          `0.0.0.0`,
+          `${slugify(example)}@dev`,
+          `--no-data`,
+        ])
+        dump = stdout
+      } else if (~example.indexOf(`postgres-`)) {
+        const { stdout } = await exec(`pg_dump`, [
+          `-U`,
+          `root`,
+          `PGPASSWORD=prisma`,
+          `-p`,
+          `5433`,
+          `-h`,
+          `0.0.0.0`,
+          `${slugify(example)}@dev`,
+          `--no-data`,
+        ])
+        dump = stdout
+      } else {
+        throw new Error('unknown database type')
+      }
+      await fs.writeFile(path.join(fullpath, 'dump.sql'), dump)
+      const schemaPrisma = path.join(fullpath, 'schema.prisma')
+      console.log(`${example}: introspecting MySQL database`)
+      await exec(
+        prisma2,
+        [
+          'introspect',
+          '--url',
+          `mysql://root:prisma@0.0.0.0:3307/${slugify(example)}@dev`,
+          `--schema`,
+          schemaPrisma,
+        ],
+        {
+          cwd,
+        },
+      )
+    }
+  } catch (e) {
+    console.log(e)
+  } finally {
+    for (dcConfigDir of dcConfigDirs) {
+      const fullpath = path.join(dcConfigPath, dcConfigDir)
+      const dcfile = path.join(fullpath, 'docker-compose.yml')
+      console.log(`shutting down docker compose ${dcConfigDir}`)
+      await exec('docker-compose', ['-f', dcfile, 'down'], { cwd })
+    }
   }
 }
 
