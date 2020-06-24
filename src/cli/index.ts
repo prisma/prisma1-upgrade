5a722a010b8e4c7b872cf3c9197e87fad90daf62
Matthew Mueller <mattmuelle@gmail.com>
2020-06-24 15:35:49 +0200

    fix message indentation

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -341,7 +341,9 @@ async function main(argv: string[]): Promise<void> {
     }
 
     console.log(
-      redent(`${bold('➤ Breaking Changes Detected')}
+      redent(`
+        ${bold('➤ Breaking Changes Detected')}
+
         We've detected some changes that will break your Prisma 1 application
         client. If you apply these changes you'll need to migrate to Prisma 2
         all at once. This will give you better schema design and performance
