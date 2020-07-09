6ae8a449297c7e39ee58d992862cf63b1b538f1b
Matthew Mueller <mattmuelle@gmail.com>
2020-07-09 14:08:59 +0200

    fix test

--- examples/mysql1-self-relation-has-many/expected.sql
+++ examples/mysql1-self-relation-has-many/expected.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `User` ADD COLUMN `invitedUserId` char(25) CHARACTER SET utf8 NOT NULL;
-UPDATE `User`, `_UserInvitation` SET `User`.`invitedUserId` = `_UserInvitation`.A where `_UserInvitation`.B = `User`.`id`;
+UPDATE `User`, `_UserInvitation` SET `User`.`invitedUserId` = `_UserInvitation`.B where `_UserInvitation`.A = `User`.`id`;
 ALTER TABLE `User` ADD CONSTRAINT invitedUser FOREIGN KEY (`invitedUserId`) REFERENCES `User`(`id`);
 DROP TABLE `_UserInvitation`;
